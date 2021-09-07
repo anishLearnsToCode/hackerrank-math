9dfbf59032e300a9c8b7b2fe2882eab78f1d8b76
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-07 16:29:25 +0530

    Added solution for B'day Gift problem (#4)
    * Added solution for B'day Gift problem
    
    * Resolved issue
    
    * Newline
    
    * NewLine added
    
    * adding lines
    
    * New line added

---
+++ cpp/bdayGift.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    cout << fixed;
+    cout << setprecision(1);
+    double total = 0;
+    for (int i = 0; i < N; i++) {
+        int cur;
+        cin >> cur;
+        total += 0.5 * cur;
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
+
