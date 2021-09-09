2e0058ed2dc610ec7804df6ea77f3949406c3beb
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-09 16:30:42 +0530

    Added Solution for Maximum Draws (#7) in C++

---
+++ cpp/maximumDraws.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << n + 1 << " \n";
+    }
+
+    return 0;
+}
+
