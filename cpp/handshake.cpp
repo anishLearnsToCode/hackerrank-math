bec6fc7bf1c92ef0b24b5a02a604e3d6c81cf9be
Rohit ranjan <ranjanrohit812@gmail.com>
2021-09-10 15:22:12 +0530

    Added solution for Handshake in C++

---
+++ cpp/handshake.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        cout << ((n - 1) * n) / 2 << endl;
+    }
+
+    return 0;
+}
+
