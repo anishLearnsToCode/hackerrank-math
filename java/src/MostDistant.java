3844d2cf18c8b62daca4930fb5d8475c9da5079d
Anish Sachdeva <anish_@outlook.com>
2022-07-26 17:08:13 +0200

    solves most distant in java

---
+++ java/src/MostDistant.java
@@ -0,0 +1,64 @@
+
+
+import java.util.Scanner;
+
+public class MostDistant {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = SCANNER.nextInt();
+        int xMin = 0, xMax = 0, yMin = 0, yMax = 0;
+
+        xMin = xMax = SCANNER.nextInt();
+        yMin = yMax = SCANNER.nextInt();
+
+        for (int i = 1 ; i < N ; i++) {
+            final int x = SCANNER.nextInt();
+            final int y = SCANNER.nextInt();
+            yMin = Math.min(yMin, y);
+            yMax = Math.max(yMax, y);
+            xMin = Math.min(xMin, x);
+            xMax = Math.max(xMax, x);
+        }
+
+        double distance = max(
+                xMax - xMin,
+                yMax - yMin,
+                distance(xMin, yMin),
+                distance(xMin, yMax),
+                distance(yMin, xMax),
+                distance(yMax, xMax)
+        );
+
+        System.out.println(toDecimalString(distance, 8));
+    }
+
+    private static double distance(int a, int b) {
+        return Math.sqrt(a * a + b * b);
+    }
+
+    private static double max(double... numbers) {
+        double result = Double.MIN_VALUE;
+        for (double number : numbers) {
+            result = Math.max(result, number);
+        }
+        return result;
+    }
+
+    public static String toDecimalString(Double number, int precision) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(number.intValue());
+        number -= number.intValue();
+
+        sb.append('.');
+
+        for (int i = 0; i < precision; i++) {
+            number *= 10;
+            sb.append(number.intValue());
+            number -= (number.intValue());
+        }
+
+        return sb.toString();
+    }
+}
